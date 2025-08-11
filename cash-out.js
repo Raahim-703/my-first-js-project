5c1110df67e071f187cd8b7f82ff2e3eec6dd111
Raahim-703 <abdurraahim703@gamil.com>
2025-08-11 11:48:43 +0600

    add two js file in my website

--- cash-out.js
+++ cash-out.js
@@ -1,11 +1,11 @@
-document.getElementById('add-money-btn')
+document.getElementById('cash-out-money-btn')
     .addEventListener('click', function (event) {
         event.preventDefault();
         const money = document.getElementById('amount').value;
         const pin = document.getElementById('pin').value;
         if (pin === '1234') {
             const balance = document.getElementById('current-balance').innerText;
-            const newBalance = parseFloat(balance) + parseFloat(money);
+            const newBalance = parseFloat(balance) - parseFloat(money);
             document.getElementById('current-balance').innerText = newBalance;
         }
     })
---
+++ transfer.js
@@ -0,0 +1,4 @@
+document.getElementById('cash-btn').addEventListener('click',function(){
+    document.getElementById('cash-part').classList.remove('hidden');
+    document.getElementById('add-part').classList.add('hidden')
+})
