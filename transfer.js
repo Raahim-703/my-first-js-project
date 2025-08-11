6a0c66296fb812a224c68d71defc5b9fd5750845
Raahim-703 <abdurraahim703@gamil.com>
2025-08-11 11:52:54 +0600

    I have finished the work

--- transfer.js
+++ transfer.js
@@ -1,4 +1,8 @@
-document.getElementById('cash-btn').addEventListener('click',function(){
+document.getElementById('cash-btn').addEventListener('click', function () {
     document.getElementById('cash-part').classList.remove('hidden');
     document.getElementById('add-part').classList.add('hidden')
+})
+document.getElementById('add-btn').addEventListener('click', function () {
+    document.getElementById('cash-part').classList.add('hidden');
+    document.getElementById('add-part').classList.remove('hidden')
 })
